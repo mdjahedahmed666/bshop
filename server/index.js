c83d41ae6bb4f5aed503a49bfcc007add9a14f6c
mdjahedahmed666 <67577098+mdjahedahmed666@users.noreply.github.com>
2023-10-21 14:28:24 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: [""],
+    "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials:true
+  }
+));
 app.use(express.json());
 
 
