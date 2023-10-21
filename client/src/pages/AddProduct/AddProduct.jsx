25d5728e3e048fa6339a2d8e2dda89c036c4864e
mdjahedahmed666 <67577098+mdjahedahmed666@users.noreply.github.com>
2023-10-21 14:07:35 +0100

    Update AddProduct.jsx

--- client/src/pages/AddProduct/AddProduct.jsx
+++ client/src/pages/AddProduct/AddProduct.jsx
@@ -17,7 +17,7 @@ const AddProduct = () => {
         const newProduct = {name, brandName, type, price, shortDescription, photo, rating}
 
         //send data to the server
-        fetch('https://server-iul79kjuo-mdjahedahmed12-gmailcom.vercel.app/products', {
+        fetch('serverbshop.vercel.app/products', {
             method: "POST",
             headers: {
                 "content-type": "application/json"
